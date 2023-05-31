49c5df58a42919dac9fd1642b0b8530675521385
Jorge Luis Pachari Quispe <90292239+JorgeLuis1907@users.noreply.github.com>
2023-05-31 13:18:38 -0500

    Create del archivo Node.java

---
+++ Node.java
@@ -0,0 +1,10 @@
+public class Node<T>{  
+    private T dato;
+    private Node<T> siguiente;
+    private int contador;
+    public Node(){
+        this.head=null;
+        this.siguiente=new Node<T>;
+    }
+
+}
